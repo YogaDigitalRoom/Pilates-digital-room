9d81fa0865f6e90738439536101bd943b936f21c
MariaGambin <info@yogadigitalroom.com>
2024-12-19 15:26:30 +0100

    blogs added

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div className="text-secondary">
+        FOOTER 
+        </div>
+     );
+}
+ 
+export default Footer;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,9 +7,29 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+
+      
+      screens: {
+				'tablet': '900px',
+			},
+			colors : {
+        "black" :"#2B2B2B",
+        "white": "#FFFFFF",	
+        "grey" : "#D9D9D9",
+        "primary" : "#92D641",
+        "secondary" : "#04A1E1",
+        "tertiary" : "#2D3178"
+      },
+      borderRadius : {
+				"primary" : "8px",
+				"secondary" : "16px",
+				"tertiary" : "24px",
+			},
+
+
+      boxShadow: {
+        main: "0px 0px 10px 2px rgba(0, 0, 0, 0.1)",  // Define primary box shadow
+        hover: "0px 0px 10px 2px rgba(0, 0, 0, 0.3)",   // Define hover box shadow
       },
     },
   },
